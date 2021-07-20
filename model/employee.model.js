5c00ea87a3cb6a12cef5ccdc13a2adf3436789a2
kwon, minho <minhob38@gmail.com>
2021-07-20 11:53:38 +0900

    Define model schema

---
+++ model/employee.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 12
+  },
+  gender: { type: String },
+  phone: { type: String },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.pluralize(null);
+const employeeModel = mongoose.model("myemployee_test", contactSchema);
+
+module.exports = employeeModel;
