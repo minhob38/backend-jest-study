8a48ae3d5a4d0e8b526e5bc2ab9fd51afb401cdd
kwon, minho <minhob38@gmail.com>
2021-07-21 18:16:01 +0900

    Add test for getEmployeeById

--- controller/employee.controller.js
+++ controller/employee.controller.js
@@ -65,6 +65,7 @@ exports.getEmployeeById = async (req, res, next) => {
       res.status(404).send("not found");
     }
   } catch (err) {
+    console.log(err)
     res.status(500).send(err);
   }
 }
