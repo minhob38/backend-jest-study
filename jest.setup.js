c6319525b2ed2fb30a7ef0dfcd078b20761e222e
kwon, minho <minhob38@gmail.com>
2021-07-23 00:17:43 +0900

    Add jest setup file

---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+console.log("Running jest.setup.js");
+jest.setTimeout(30 * 1000);
+const a = 1;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const mongodb = require("./mongodb/mongodb.utils");
 const app = require("./app");
 const port = 8000;
+require("dotenv").config();
+
+console.log("!!!")
+console.log(typeof process.env.ENV)
 
 mongodb.connect();
 
